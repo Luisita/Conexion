7f8cf459f439861fe04fc965e6fd8e9b31db94d5
Luisita <luiyi_1414@hotmail.com>
2015-02-08 16:41:10 -0600

    5

--- www/js/app.js
+++ www/js/app.js
@@ -1,26 +1,14 @@
 // función para probar la conectividad
-$('#formulario').submit(function() {
-	
-	//texto = checkConnection();
-	$("#tipo").val("Hola");
-	$.mobile.changepage("#resultado")
-});
-
 function checkConnection() {
-		var networkState = navigator.network.connection.type;
-		var states = {};
-		states[Connection.UNKNOWN] = 'Unknown connection';
-		states[Connection.ETHERNET] = 'Ethernet connection';
-		states[Connection.WIFI] = 'WiFi connection';
-		states[Connection.CELL_2G] = 'Cell 2G connection';
-		states[Connection.CELL_3G] = 'Cell 3G connection';
-		states[Connection.CELL_4G] = 'Cell 4G connection';
-		states[Connection.NONE] = 'No network connection';
-		var txt = states[networkState];
-		return txt;
+	var networkState = navigator.network.connection.type;
+	var states = {};
+	states[Connection.UNKNOWN] = 'Unknown connection';
+	states[Connection.ETHERNET] = 'Ethernet connection';
+	states[Connection.WIFI] = 'WiFi connection';
+	states[Connection.CELL_2G] = 'Cell 2G connection';
+	states[Connection.CELL_3G] = 'Cell 3G connection';
+	states[Connection.CELL_4G] = 'Cell 4G connection';
+	states[Connection.NONE] = 'No network connection';
+	var txt = "<p>Tipo de conexi&oacuten: "+states[networkState]+"</p>";
+	$("#testC").html(txt);
 }
-
-$('#regresar').submit(function() {
-	$("#tipo").val("");
-	$.mobile.changepage("#index")
-});
