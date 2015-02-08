8bb645945b155ddae0a365ccf89790c5ef2760ef
Luisita <luiyi_1414@hotmail.com>
2015-02-08 16:27:00 -0600

    3

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,26 @@
 // función para probar la conectividad
+$('#formulario').submit(function() {
+	
+	texto = checkConnection();
+	$("#tipo").val(texto);
+	$.mobile.changepage("#resultado")
+});
+
 function checkConnection() {
-	var networkState = navigator.network.connection.type;
-	var states = {};
-	states[Connection.UNKNOWN] = 'Unknown connection';
-	states[Connection.ETHERNET] = 'Ethernet connection';
-	states[Connection.WIFI] = 'WiFi connection';
-	states[Connection.CELL_2G] = 'Cell 2G connection';
-	states[Connection.CELL_3G] = 'Cell 3G connection';
-	states[Connection.CELL_4G] = 'Cell 4G connection';
-	states[Connection.NONE] = 'No network connection';
-	var txt = "<p>Tipo de conexi&oacuten: "+states[networkState]+"</p>";
-	$("#tipo").html("Hola");
-	$.mobile.changePage("#resultado")
+		var networkState = navigator.network.connection.type;
+		var states = {};
+		states[Connection.UNKNOWN] = 'Unknown connection';
+		states[Connection.ETHERNET] = 'Ethernet connection';
+		states[Connection.WIFI] = 'WiFi connection';
+		states[Connection.CELL_2G] = 'Cell 2G connection';
+		states[Connection.CELL_3G] = 'Cell 3G connection';
+		states[Connection.CELL_4G] = 'Cell 4G connection';
+		states[Connection.NONE] = 'No network connection';
+		var txt = states[networkState];
+		return txt;
 }
 
-function Regresar() {
-	
-	$("#tipo").html("");
-	$.mobile.changepage("#index");
-}
+$('#regresar').submit(function() {
+	$("#tipo").val("");
+	$.mobile.changepage("#index")
+});
