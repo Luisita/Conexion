a12c461eccd670428c1e22a0c502d304aa333f6c
Luisita <luiyi_1414@hotmail.com>
2015-02-08 17:19:08 -0600

    9

--- www/js/app.js
+++ www/js/app.js
@@ -11,5 +11,5 @@ function checkConnection() {
 	states[Connection.CELL_4G] = 'Cell 4G connection';
 	states[Connection.NONE] = 'No network connection';
 	var txt = "<p>Tipo de conexi&oacuten: "+states[networkState]+"</p>";
-	$.mobile.changePage("#tipo")
+	alert("Tipo de conexion"+"<br>"+text);
 }
